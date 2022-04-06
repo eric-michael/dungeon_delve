a76413edfcc5578bd5ceb075e7e89b31d191c9b1
eric-michael <hicks.ericmichael@gmail.com>
2022-04-05 21:26:50 -0400

    Remove some artifacts

--- server/index.js
+++ server/index.js
@@ -10,10 +10,6 @@ app.listen(port, console.log(`Listening on port ${port}`));
 
 app.get("/api/careers", routes.getAllCareers);
 
-app.get("/api/test", routes.test);
-
-app.get("/api/test2", routes.test2);
-
 const generateGame = require("./scripts/generateGame").generateGame;
 
 const starting_career = require("./store/player/careerStart").career_starts;
@@ -21,7 +17,5 @@ const starting_career = require("./store/player/careerStart").career_starts;
 app.post("/api/generateGame", (req, res) => {
     res.send({ message: "generate game route", dungeon: generateGame(req.body.seed, req.body.career_id), career: starting_career[req.body.career_id] });
   });
-
-const generateSeed = require("./scripts/generateSeed");
 
 module.exports = app;
