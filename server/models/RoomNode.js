f2456f2024a0cf4c211cde8807b6e6a1654e6e36
Eric Michael Hicks <ericmichael8192@gmail.com>
2022-04-27 22:12:38 -0400

    Add type_id and name prop to RoomNode

--- server/models/RoomNode.js
+++ server/models/RoomNode.js
@@ -1,13 +1,14 @@
+const room_types = require("../store/room_types").room_types;
 /**
  * Node for a linked list
  *  There is no limit for the amount of parents at the moment so we'll keep it an array
  * */
-
 class RoomNode {
-  constructor(room_id, parent_ids = [], type_id = 0, set = []) {
+  constructor(room_id, parent_ids = [], type_id = 0, name = '', set = []) {
     this.room_id = room_id;
     this.parent_ids = parent_ids;
     this.type_id = type_id;
+    this.name = name;
     this.set = set;
   }
 
@@ -17,6 +18,11 @@ class RoomNode {
 
   setTypeId(type_id) {
     this.type_id = type_id;
+    this.setName(room_types[type_id].name);
+  }
+
+  setName(name){
+    this.name = name;
   }
 
   addToSet(set){
