81bf9d804585911b65636e6a99d62367f7c0cf69
Eric Michael Hicks <ericmichael8192@gmail.com>
2022-05-06 00:32:59 -0400

    Some scaffolding

---
+++ client/src/game/room/combat/combat.js
@@ -0,0 +1,13 @@
+import ActionBar from "./actionBar";
+import DiscardPile from "./discardPile";
+import DrawPile from "./drawPile";
+import Enemy from "./enemy";
+import EnemyContainer from "./enemyContainer";
+import hand from "./hand";
+import player from "./player";
+
+function Combat() {
+  return <div>Combat</div>;
+}
+
+export default Combat;
