2e5a07e264a36263d0bffacfb41b2c5228928b21
eric-michael <hicks.ericmichael@gmail.com>
2023-04-16 13:32:04 -0400

    Remove 'test' endpoint

--- server/controllers/career.js
+++ server/controllers/career.js
@@ -3,7 +3,3 @@ const careerModel = require("../models/career").Career;
 exports.getAllCareers = (req, res) => {
   res.send(careerModel.getAllCareers());
 };
-
-exports.test = (req, res) => {
-  res.send({ message: "server working" });
-};
