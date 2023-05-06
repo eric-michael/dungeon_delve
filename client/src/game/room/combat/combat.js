364d34e761763c894ab4f0b56c56c098b2b9c1d7
eric-michael <hicks.ericmichael@gmail.com>
2023-05-06 14:50:09 -0400

    Connect combat and player components

--- client/src/game/room/combat/combat.js
+++ client/src/game/room/combat/combat.js
@@ -1,13 +1,34 @@
+/*
+  Container for all of the combat room components
+  Probably should handle all of the combat actions, and pass data to the child components.
+*/
+
 import ActionBar from "./combat_components/actionBar";
 import DiscardPile from "./combat_components/discardPile";
 import DrawPile from "./combat_components/drawPile";
 import Enemy from "./combat_components/enemy";
 import EnemyContainer from "./combat_components/enemyContainer";
-import hand from "./combat_components/hand";
-import player from "./combat_components/player";
+import Hand from "./combat_components/hand";
+import Player from "./combat_components/player";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import { playerActions } from "../../../store/index";
 
 function Combat() {
-  return <div>Combat</div>;
+
+  let player_data = {};
+
+  player_data.current_hp = useSelector((state) => state.player.current_hp);
+  player_data.max_hp = useSelector((state) => state.player.max_hp);
+  player_data.energy = useSelector((state) => state.player.energy);
+
+  return (
+    <div>
+      Combat
+      <Player player_data={player_data}/>
+    </div>
+  );
 }
 
 export default Combat;
--- client/src/game/room/combat/combat_components/player.js
+++ client/src/game/room/combat/combat_components/player.js
@@ -1,11 +1,21 @@
 /* 
+    Player Sprite*
     Player status
 
     HP, buffs, equipment, etc.
 */
 
-function Player(){
-    return <div>Player</div>
+function Player(props){
+    let current_hp = props.player_data.current_hp;
+    let max_hp = props.player_data.max_hp;
+    let max_energy = props.player_data.energy;
+    return (
+    <div>
+        Player
+        Health: {current_hp}/{max_hp}
+        Max Energy: {max_energy}
+    </div>
+    );
 }
 
 export default Player
